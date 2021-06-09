7f490b6fcc8c37c522625d5c57af219830240424
alexcortes22 <alex.cortes47@qmail.cuny.edu>
2021-06-09 16:44:27 -0400

    Correcting Insertion Sort
    Added test case in main method

--- Algorithms/InsertionSort.java
+++ Algorithms/InsertionSort.java
@@ -2,20 +2,34 @@ public class InsertionSort{
 
     /*
         This method displays the InsertionSort algorithm.
-        @param arr This is the array to be sroted
+        @param arr This is the array to be sorted
         @return int[]. The array that was input, sorted in ascending order 
 
     */
-
     public int[] InsertionSort(int[] arr){
-        for(int j = 2; j < arr.length; j++){
-            int key = arr[j];
-            int i = j-1; 
-            while (i > 0 & arr[i] > key){
-                arr[i+1] = a[i];
-                i--;
+        for(int j = 1; j < arr.length; j++){
+            int current = arr[j];
+            int i = j - 1; 
+            //while loop is within bounds of array and the current num is greater than previous num
+            while (i >= 0 && arr[i] > current){
+                arr[i + 1] = arr[i];
+                i = i - 1; //this is what moves elements to the left
             }
-            a[i+1] = key;
+            arr[i + 1] = current;
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args){
+        InsertionSort demo = new InsertionSort();
+
+        int[] nums = {5,2,4,6,1,3};
+
+        int[] sorted = demo.InsertionSort(nums);
+
+        for(int i = 0; i < sorted.length; i++){
+            System.out.println(sorted[i]);
         }
     }
 }

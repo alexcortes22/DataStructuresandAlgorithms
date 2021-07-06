6858b85cb5c3a1763089150e5d92543a3c75bb8e
Alex Cortes <alex.cortes2137@gmail.com>
2021-07-06 08:24:48 -0400

    gridTraveler Tabulation

--- Algorithms/tabulation.java
+++ Algorithms/tabulation.java
@@ -19,7 +19,28 @@ public class tabulation {
         return array[n];
     }
 
+    /*
+        gridTraveler: A function that determines the number of ways to get from the upper left corner
+        to the lower right corner.
+        Complexity: 
+                    Time: O(m * n);
+                    Space: O(m * n);
+    */
+    public static int gridTraveler(int m, int n){
+        int[][] table = new int[m + 1][n + 1];
+        table[1][1] = 1;
+        for(int i = 0; i <= m; i++){
+            for(int j = 0; j <= n; j++){
+                int current = table[i][j];
+                if(j + 1 <= n) table [i][j + 1] += current; //add current to the right
+                if(i + 1 <= m) table [i + 1][j] += current; //add current to the left
+            }
+        }
+        return table[m][n];
+    }
+
     public static void main(String[] args){
         System.out.println(fib(6)); //should return 8
+        System.out.println(gridTraveler(3,3)); //should return 6
     }
 }

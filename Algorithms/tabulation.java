c1284b6236b98574fae8f1c6cdc6b1b2723ae191
Alex Cortes <alex.cortes2137@gmail.com>
2021-07-06 09:00:14 -0400

    canSum tabulation

--- Algorithms/tabulation.java
+++ Algorithms/tabulation.java
@@ -39,8 +39,32 @@ public class tabulation {
         return table[m][n];
     }
 
+    /*
+        canSum: Given a target sum and an array of numbers, is it possible to add numbers to get sum.
+        Complexity:
+                    Time: O(m * n);
+                    Space: O(m);
+    */
+    public static boolean canSum(int targetSum, int[] numbers){
+        Boolean[] table = new Boolean[targetSum + 1];
+        Arrays.fill(table, false);
+        table[0] = true; //base case. If its 0, you can add it with numbers.
+
+        for(int i = 0; i < targetSum - 1; i++){
+            if(table[i]){
+                for(int j = 0; j < numbers.length; j++){
+                    table[i + j] = true;
+                }
+            }
+        }
+
+        return table[targetSum];
+    }
+
     public static void main(String[] args){
         System.out.println(fib(6)); //should return 8
         System.out.println(gridTraveler(3,3)); //should return 6
+        int[] nums = {5,4,3};
+        System.out.println(canSum(7, nums)); // should return true
     }
 }
